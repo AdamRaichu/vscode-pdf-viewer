ab4a2a6e61f47e7d309054fcc013b130e93377e4
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-13 20:19:26 +0000

    fix require("core")

--- .actions/checks.js
+++ .actions/checks.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const core = require("@actions/core");
 
 module.exports = function () {
   fs.readFile(".actions/cached-version", "utf8", (err, v) => {
@@ -17,7 +18,9 @@ module.exports = function () {
         package_raw.version = [oV[0], oV[1], oV[2] + 1].join("");
         fs.writeFileSync("package.json", JSON.stringify(package_raw, null, 2), "utf8");
       }
-      fs.writeFile(".actions/cached-version", package_raw.version, "utf8", () => {});
+      fs.writeFile(".actions/cached-version", package_raw.version, "utf8", () => {
+        return package_raw.version;
+      });
     });
   });
 };
